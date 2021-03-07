f3b955d33bddd12f4adf9b71b50957b51e3901a7
Devashni <45024120+devashni@users.noreply.github.com>
2021-03-07 11:33:12 -0800

    this is pre final version of the full project

---
+++ background.js
@@ -0,0 +1,41 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(
+    tab.id,
+   {file: 'run_in_page.js'});  
+});
+
+// Add listener for messages from the script we inject
+// and run inside the active webpage.
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+
+    // full text of the webpage is in the parameter to this message
+    // which we can access via request.parameterName
+    let fullPageText = request.pageText;
+
+    // Code to change icon of the extension.
+    let score = 100;
+    if (score == 1) {
+      // very negative
+      chrome.browserAction.setIcon({path: "images/Sad.png"}, function() {
+      alert("Are you sure want to read this right now? This page is sad. :( ")
+      });
+      // very positive
+    } else if (score == 100) {
+      chrome.browserAction.setIcon({path: "images/Smile.png"})
+      alert("This is a happy page if you need a pick me up. :D ")
+    }
+    // super neutral
+    else {
+      chrome.browserAction.setIcon({path: "images/Smile.png"})
+      alert("This page is neutral. Meh. :-| ")
+    }
+
+    if (request.color) {
+      sendResponse({});
+    }
+  }
+);
---
+++ content.js
@@ -0,0 +1,21 @@
+const YOUR_API_KEY = 'fill this in';
+
+const gCloudSentimentURL = `https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=${YOUR_API_KEY}`;
+
+const body = {
+  document: {
+    type: 'PLAIN_TEXT',
+    language: 'EN',
+    content:
+      'The supposed honeymoon period of a new president would typically provide a moment for lawmakers to come together, particularly as the nation enters its second year of a crushing health and economic crisis. Instead, the tense showdown over the stimulus legislation showed that lawmakers were pulling apart, and poised for more ugly clashes ahead.',
+  },
+  encodingType: 'UTF8',
+};
+
+fetch(gCloudSentimentURL, {
+  method: 'POST',
+  headers: {},
+  body: JSON.stringify(body),
+})
+  .then((response) => response.json())
+  .then((response) => console.log(response));
